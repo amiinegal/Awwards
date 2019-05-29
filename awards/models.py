ca411838eda789298517531995600b705e3ec47e
amiinegal <you@example.com>
2019-05-29 10:44:17 +0300

    create project and profile

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()    
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='picture/')
+    bio = models.TextField(max_length=200, null=True, blank=True, default="bio")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self):
+        self.delete()
