1a76dfb68334f97469f9ef96849fa960e8979319
amiinegal <you@example.com>
2019-05-29 15:18:03 +0300

    create form for search

---
+++ awards/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile, Project
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text = 'Required')
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+       
+        exclude = ['user_profile']
+
+# class ReviewForm(forms.ModelForm):
+#     class Meta:
+#         model = Reviews
+#         exclude = ['user','project','user_rating']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
